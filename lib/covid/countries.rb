0118821e2d690471b8526c1fac6ec091248de238
erinstansbury <ekstansbury@gmail.com>
2020-06-27 17:31:08 -0400

    remove comments

--- lib/covid/countries.rb
+++ lib/covid/countries.rb
@@ -20,13 +20,4 @@ class Covid::Countries
     def self.top_sorted
         @all.take(10).sort
     end
-
-    #countries should have a name attr, another option for example list alphabetically 
-
-#     def self.top
-#         countries = Covid::Scraper.scrape_countries
-#         top = countries.slice!(0,10)
-#         top
-#     end
-
 end
